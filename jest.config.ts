fcd98450c5e166041bd531b72860948cb2906546
Alvin Setiawan <alvin.set25@gmail.com>
2025-02-02 03:11:44 +0700

    refactor(config): add moduleNameMapper jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -8,8 +8,8 @@ import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: './'
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -96,7 +96,13 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    // '^@/(.*)$': '<rootDir>/src/$1',
+    '@/components/ui/alert-dialog': '<rootDir>/src/components/ui/alert-dialog',
+    '@/components/ui/button': '<rootDir>/src/components/ui/button',
+    '@/components/ui/sonner': '<rootDir>/src/components/ui/sonner',
+    '@/lib/utils': '<rootDir>/src/lib/utils'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -164,7 +170,7 @@ const config: Config = {
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
+  ]
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
