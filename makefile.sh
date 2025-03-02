e522913b911fc1d2e82cd5b2a2f9eaf9138e2454
Alvin Setiawan <alvin.set25@gmail.com>
2025-03-02 12:24:42 +0700

    feat(package): add package and add script makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -7,8 +7,15 @@ clean_install() {
   npm install
 }
 
+clean_build() {
+  echo "Delete Build Folder"
+  rm -rf .next
+}
+
 if [ "$1" = "clean_install" ]; then
   clean_install
+elif [ "$1" = "clean_build" ]; then
+  clean_build
 else
   echo "Invalid function name. Available functions: 'clean_install'"
 fi
