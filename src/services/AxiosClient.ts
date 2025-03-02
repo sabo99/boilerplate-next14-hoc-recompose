45c3918b38cf211b2ef800c5936bed0674eefc85
Alvin Setiawan <alvin.set25@gmail.com>
2025-03-02 15:07:34 +0700

    feat(service): add boilerplate axios client service

---
+++ src/services/AxiosClient.ts
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+// class AxiosClient {
+//   _client: object;
+
+//   _erroHandler: Function;
+
+//   _requestHandler: Function;
+
+//   constructor(baseURL: string, withCredentials: boolean) {
+//     this._client = axios.create({
+//       baseURL,
+//     })
+//   }
+// }
