060f6a7a22cbde1e88373d46d13048783a02cfe8
Alvin Setiawan <alvin.set25@gmail.com>
2025-03-11 23:22:00 +0700

    feat(mocks): update mock lucide-react

--- src/__mocks__/lucide-react.tsx
+++ src/__mocks__/lucide-react.tsx
@@ -1,3 +1,11 @@
 const MockIcon = (props: any) => <div {...props}>Mock Icon</div>;
 
 export const LinkIcon = MockIcon;
+export const ChevronRightIcon = MockIcon;
+export const ViewIcon = MockIcon;
+export const LayoutDashboardIcon = MockIcon;
+export const SquareTerminalIcon = MockIcon;
+export const LoaderIcon = MockIcon;
+export const MonitorDotIcon = MockIcon;
+export const RefreshCwOffIcon = MockIcon;
+export const DatabaseZapIcon = MockIcon;
