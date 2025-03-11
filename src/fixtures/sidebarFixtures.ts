2bace8f5e0328acbe9ae6a56253357daa8ea00c4
Alvin Setiawan <alvin.set25@gmail.com>
2025-03-11 23:21:32 +0700

    feat(fixtures): add fixtures

---
+++ src/fixtures/sidebarFixtures.ts
@@ -0,0 +1,62 @@
+import {
+  DatabaseZapIcon,
+  LayoutDashboardIcon,
+  LoaderIcon,
+  MonitorDotIcon,
+  RefreshCwOffIcon,
+  SquareTerminalIcon,
+  ViewIcon
+} from '@/__mocks__/lucide-react';
+
+const navMainItemData = [
+  {
+    title: 'Overviews',
+    url: '/Dashboard',
+    icon: ViewIcon,
+    isActive: true,
+    subItems: [
+      {
+        title: 'Dashboard',
+        url: '/Dashboard',
+        icon: LayoutDashboardIcon,
+        permission: 'VIEW_DASHBOARD'
+      }
+    ]
+  },
+  {
+    title: 'Examples',
+    url: '#',
+    icon: SquareTerminalIcon,
+    isActive: true,
+    subItems: [
+      {
+        title: 'withLoadingOverlay',
+        url: '/with-loading-overlay',
+        icon: LoaderIcon,
+        permission: 'VIEW_LOADING_OVERLAY'
+      },
+      {
+        title: 'withIdleOverlay',
+        url: '/with-idle-overlay',
+        icon: MonitorDotIcon,
+        permission: 'VIEW_IDLE_OVERLAY'
+      },
+      {
+        title: 'withPreventRefresh',
+        url: '#',
+        icon: RefreshCwOffIcon,
+        permission: 'VIEW_PREVENT_REFRESH'
+      },
+      {
+        title: 'withDataFetching',
+        url: '#',
+        icon: DatabaseZapIcon,
+        permission: 'VIEW_DATA_FETCHING'
+      }
+    ]
+  }
+];
+
+export {
+  navMainItemData
+};
