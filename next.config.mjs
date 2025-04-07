b73e387e6a1eddbaf4ce3d13da1cca0cea732367
Alvin Setiawan <alvin.set25@gmail.com>
2025-04-07 21:25:42 +0700

    feat(config): add crypto keys with cert

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import fs from "fs";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	/* config options here */
+	env: {
+		CRYPTO_HASHED_KEY: fs.readFileSync(process.env.NEXT_PUBLIC_CRYPTO_HASHED_KEY_PATH || '', 'utf8'),
+		CRYPTO_ENCRYPTED_KEY: fs.readFileSync(process.env.NEXT_PUBLIC_CRYPTO_ENCRYPTED_KEY_PATH || '', 'utf8'),
+	},
+};
 
 export default nextConfig;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,18 @@
+const crypto = {
+  hashedKey: process.env.CRYPTO_HASHED_KEY as string,
+  encryptedKey: process.env.CRYPTO_ENCRYPTED_KEY as string
+};
+
 const Config = {
-  env: process.env.APP_ENV,
+  env: process.env.APP_ENV as string,
   api: {
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL as string
   },
   headers: {
-    accessTokenKey: process.env.NEXT_PUBLIC_AUTH_KEY
+    accessTokenKey: process.env.NEXT_PUBLIC_AUTH_KEY as string
   },
-  auth: {
-    publicKeyPath: '',
-    privateKeyPath: ''
+  secret: {
+    crypto
   }
 };
 
