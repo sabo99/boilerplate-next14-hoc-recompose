32ad326a8edaaecf29e912d8c3998d2009fc1495
Alvin Setiawan <alvin.set25@gmail.com>
2025-04-09 20:43:30 +0700

    feat(config): add injection env on next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import fs from "fs";
+import fs from 'fs';
 
 /**
  * @type {import('next').NextConfig}
@@ -8,8 +8,8 @@ const nextConfig = {
 	/* config options here */
 	env: {
 		CRYPTO_HASHED_KEY: fs.readFileSync(process.env.NEXT_PUBLIC_CRYPTO_HASHED_KEY_PATH || '', 'utf8'),
-		CRYPTO_ENCRYPTED_KEY: fs.readFileSync(process.env.NEXT_PUBLIC_CRYPTO_ENCRYPTED_KEY_PATH || '', 'utf8'),
-	},
+		CRYPTO_ENCRYPTED_KEY: fs.readFileSync(process.env.NEXT_PUBLIC_CRYPTO_ENCRYPTED_KEY_PATH || '', 'utf8')
+	}
 };
 
 export default nextConfig;
